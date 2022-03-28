6f5c3a42f6b83cb4a1b8cd32ddec9950190fb0f4
Ta Duy <tpduy2001@gmail.com>
2022-03-28 10:46:34 +0700

    Additional shortcut for mod change

--- src/game.py
+++ src/game.py
@@ -28,25 +28,29 @@ class Game(Application):
                         case pg.K_ESCAPE:
                             self.close()
                         case pg.K_z:
-                            if pg.key.get_mods() & pg.KMOD_CTRL:
+                            if evt.mod & pg.KMOD_CTRL:
                                 self.action_manager.undo()
                         case pg.K_y:
-                            if pg.key.get_mods() & pg.KMOD_CTRL:
+                            if evt.mod & pg.KMOD_CTRL:
                                 self.action_manager.redo()
                         case pg.K_1 | pg.K_2 | pg.K_3 | pg.K_4 | pg.K_5 | pg.K_6 | pg.K_7 | pg.K_8 | pg.K_9:
-                            old_values = self.board.fill_tiles(evt.key - pg.K_0, Board.INPUT_MODE_VALUE)
+                            mode = (evt.mod & pg.KMOD_SHIFT) | (bool(evt.mod & pg.KMOD_CTRL) << 1)
+
+                            old_values = self.board.fill_tiles(evt.key - pg.K_0, mode)
                             self.action_manager.new_action(BoardInputAction(
                                 self.board,
                                 evt.key - pg.K_0,
-                                Board.INPUT_MODE_VALUE,
+                                mode,
                                 old_values
                             ))
                         case pg.K_KP1 | pg.K_KP2 | pg.K_KP3 | pg.K_KP4 | pg.K_KP5 | pg.K_KP6 | pg.K_KP7 | pg.K_KP8 | pg.K_KP9:
-                            old_values = self.board.fill_tiles(evt.key - pg.K_KP1 + 1, Board.INPUT_MODE_COLOR)
+                            mode = (evt.mod & pg.KMOD_SHIFT) | (bool(evt.mod & pg.KMOD_CTRL) << 1)
+
+                            old_values = self.board.fill_tiles(evt.key - pg.K_KP1 + 1, mode)
                             self.action_manager.new_action(BoardInputAction(
                                 self.board,
                                 evt.key - pg.K_KP1 + 1,
-                                Board.INPUT_MODE_COLOR,
+                                mode,
                                 old_values
                             ))
                 case pg.MOUSEBUTTONDOWN:
