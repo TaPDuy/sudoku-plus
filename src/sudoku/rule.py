cdc2806acec0212194f77f1834838b2824d16a24
TaPDuy <tpduy2001@gmail.com>
2022-04-03 15:00:46 +0700

    Added row and box rules

--- src/sudoku/rule.py
+++ src/sudoku/rule.py
@@ -1,5 +1,7 @@
 class Rule:
-    pass
+
+    def conflict(self, *args) -> bool:
+        pass
 
 
 class GlobalRule(Rule):
@@ -12,6 +14,28 @@ class ComponentRule(Rule):
 
 class ColumnRule(GlobalRule):
 
-    @staticmethod
-    def conflict(p1: tuple[int, int], p2: tuple[int, int]) -> bool:
+    def conflict(self, p1: tuple[int, int], p2: tuple[int, int]) -> bool:
         return p1[0] == p2[0]
+
+
+class RowRule(GlobalRule):
+
+    def conflict(self, p1: tuple[int, int], p2: tuple[int, int]) -> bool:
+        return p1[1] == p2[1]
+
+
+class BoxRule(GlobalRule):
+
+    def conflict(self, p1: tuple[int, int], p2: tuple[int, int]) -> bool:
+        return p1[0] // 3 == p2[0] // 3 and p1[1] // 3 == p2[1] // 3
+
+
+class SudokuRule(GlobalRule):
+
+    def __init__(self):
+        self.sub_rules: list[Rule] = [ColumnRule(), RowRule(), BoxRule()]
+
+    def conflict(self, p1: tuple[int, int], p2: tuple[int, int]) -> bool:
+        return self.sub_rules[0].conflict(p1, p2) or \
+               self.sub_rules[1].conflict(p1, p2) or \
+               self.sub_rules[2].conflict(p1, p2)
