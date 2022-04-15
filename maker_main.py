7986965cc4e27da0d9462d23a39bf8c7d48654a9
TaPDuy <tpduy2001@gmail.com>
2022-04-15 19:55:42 +0700

    Added level maker

---
+++ maker_main.py
@@ -0,0 +1,9 @@
+from src import LevelMaker
+
+
+def main():
+    LevelMaker().run()
+
+
+if __name__ == '__main__':
+    main()
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
+from .app import Application
 from .game import Game
+from .maker import LevelMaker
 from .core.utils import constants
