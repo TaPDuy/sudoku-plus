4446e6e80455e5fd16e29193af146a30ee15351e
TaPDuy <tpduy2001@gmail.com>
2022-05-08 10:04:02 +0700

    Removed margin and fixed gap

--- src/sudoku/input.py
+++ src/sudoku/input.py
@@ -18,48 +18,55 @@ class InputPanel(UIPanel):
     BUTTON_CHECK = 15
 
     def __init__(self, rect: Rect, gap: tuple[float, float], manager: IUIManagerInterface, container=None):
-        super().__init__(rect, 0, manager, container=container)
+        super().__init__(
+            rect, 0, manager, container=container,
+            margins={'left': 0, 'right': 0, 'top': 0, 'bottom': 0}
+        )
 
         self.gap = self.vgap, self.hgap = gap
-        self.button_box_size = (rect.w / 4, rect.h / 4)
-        self.button_size = (rect.w / 4 - 2 * self.hgap, rect.h / 4 - 2 * self.vgap)
+        self.button_size = ((rect.w - 5 * self.hgap) / 4, (rect.h - 5 * self.vgap) / 4)
 
         self.buttons = [UIButton(Rect(
             (
-                x * self.button_box_size[0] + self.hgap,
-                (2 - y) * self.button_box_size[1] + self.vgap
+                x * self.button_size[0] + (x + 1) * self.hgap,
+                (2 - y) * self.button_size[1] + (3 - y) * self.vgap
             ), self.button_size
         ), str(x + y * 3 + 1), manager, self) for y in range(3) for x in range(3)]
 
         self.buttons.append(UIButton(Rect(
-            (3 * self.button_box_size[0] + self.hgap, 0),
+            (3 * self.button_size[0] + 4 * self.hgap, self.vgap),
             self.button_size
         ), "value", manager, self))
         self.buttons.append(UIButton(Rect(
-            (3 * self.button_box_size[0] + self.hgap, self.button_box_size[1] + self.vgap),
+            (3 * self.button_size[0] + 4 * self.hgap, self.button_size[1] + 2 * self.vgap),
             self.button_size
         ), "mark", manager, self))
         self.buttons.append(UIButton(Rect(
-            (3 * self.button_box_size[0] + self.hgap, 2 * self.button_box_size[1] + self.vgap),
+            (3 * self.button_size[0] + 4 * self.hgap, 2 * self.button_size[1] + 3 * self.vgap),
             self.button_size
         ), "color", manager, self))
 
         self.buttons.append(UIButton(Rect(
-            (0, 3 * self.button_box_size[1] + self.vgap),
+            (self.hgap, 3 * self.button_size[1] + 4 * self.vgap),
             self.button_size
         ), "undo", manager, self))
         self.buttons.append(UIButton(Rect(
-            (self.button_box_size[0] + self.hgap, 3 * self.button_box_size[1] + self.vgap),
+            (self.button_size[0] + 2 * self.hgap, 3 * self.button_size[1] + 4 * self.vgap),
             self.button_size
         ), "redo", manager, self))
         self.buttons.append(UIButton(Rect(
-            (2 * self.button_box_size[0] + self.hgap, 3 * self.button_box_size[1] + self.vgap),
+            (2 * self.button_size[0] + 3 * self.hgap, 3 * self.button_size[1] + 4 * self.vgap),
             self.button_size
         ), "reset", manager, self))
         self.buttons.append(UIButton(Rect(
-            (3 * self.button_box_size[0] + self.hgap, 3 * self.button_box_size[1] + self.vgap),
+            (3 * self.button_size[0] + 4 * self.hgap, 3 * self.button_size[1] + 4 * self.vgap),
             self.button_size
         ), "check", manager, self))
+
+        for btn in self.buttons:
+            btn.shadow_width = 0
+            btn.border_width = 0
+            btn.rebuild()
 
         self.toggle_highlight_button(InputPanel.BUTTON_VALUE)
         self.functions = {}
