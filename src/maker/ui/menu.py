a9b18c498ac87e65a5482ea47382fe77d1ad84a8
TaPDuy <tpduy2001@gmail.com>
2022-05-09 17:06:52 +0700

    Menu is now responsive

--- src/maker/ui/menu.py
+++ src/maker/ui/menu.py
@@ -4,8 +4,31 @@ from pygame_gui.elements import UIPanel, UIButton
 
 
 class Menu(UIPanel):
-    def __init__(self, pos: tuple[float, float], manager: IUIManagerInterface, container=None):
-        super().__init__(Rect(pos, (300, 30)), 0, manager, container=container)
-        self.save_btn = UIButton(Rect(0, 0, 100, 30), "Save", manager, self)
-        self.open_btn = UIButton(Rect(100, 0, 100, 30), "Open", manager, self)
-        self.new_btn = UIButton(Rect(200, 0, 100, 30), "New", manager, self)
+    def __init__(self, rect: Rect, manager: IUIManagerInterface, container=None, gap=5):
+        super().__init__(
+            rect, 0, manager, container=container,
+            margins={'left': 0, 'right': 0, 'top': 0, 'bottom': 0}
+        )
+
+        btnw, btnh = (rect.w - 4 * gap) / 3, rect.h - 2 * gap
+
+        self.save_btn = UIButton(Rect(
+            gap, gap, btnw, btnh
+        ), "Save", manager, self)
+        self.save_btn.shadow_width = 0
+        self.save_btn.border_width = 0
+        self.save_btn.rebuild()
+
+        self.open_btn = UIButton(Rect(
+            btnw + 2 * gap, gap, btnw, btnh
+        ), "Open", manager, self)
+        self.open_btn.shadow_width = 0
+        self.open_btn.border_width = 0
+        self.open_btn.rebuild()
+
+        self.new_btn = UIButton(Rect(
+            2 * btnw + 3 * gap, gap, btnw, btnh
+        ), "New", manager, self)
+        self.new_btn.shadow_width = 0
+        self.new_btn.border_width = 0
+        self.new_btn.rebuild()
